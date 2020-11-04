a641835631907a43469c3596b26a1eadc97d2c10
Fredric <Fredric@DESKTOP-OJT2DFD>
2020-11-04 12:01:55 +0100

    Add project files.

---
+++ WebScraper/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WebScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxDisplayURLs = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelStatus.Location = new System.Drawing.Point(10, 602);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(186, 32);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "Found 0 images";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.BackColor = System.Drawing.Color.LightGray;
+            this.buttonSave.Enabled = false;
+            this.buttonSave.FlatAppearance.BorderSize = 0;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSave.Location = new System.Drawing.Point(921, 594);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(99, 49);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.textBoxInput);
+            this.panel1.Location = new System.Drawing.Point(12, 11);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(9);
+            this.panel1.Size = new System.Drawing.Size(903, 49);
+            this.panel1.TabIndex = 6;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.BackColor = System.Drawing.Color.LightGray;
+            this.textBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxInput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(4, 9);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(899, 32);
+            this.textBoxInput.TabIndex = 8;
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtract.BackColor = System.Drawing.Color.LightGray;
+            this.buttonExtract.FlatAppearance.BorderSize = 0;
+            this.buttonExtract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExtract.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.buttonExtract.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.buttonExtract.Location = new System.Drawing.Point(921, 11);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(99, 49);
+            this.buttonExtract.TabIndex = 7;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = false;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.textBoxDisplayURLs);
+            this.panel2.Location = new System.Drawing.Point(12, 86);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(9);
+            this.panel2.Size = new System.Drawing.Size(1009, 500);
+            this.panel2.TabIndex = 8;
+            // 
+            // textBoxDisplayURLs
+            // 
+            this.textBoxDisplayURLs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDisplayURLs.BackColor = System.Drawing.Color.LightGray;
+            this.textBoxDisplayURLs.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxDisplayURLs.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDisplayURLs.Location = new System.Drawing.Point(0, 0);
+            this.textBoxDisplayURLs.Multiline = true;
+            this.textBoxDisplayURLs.Name = "textBoxDisplayURLs";
+            this.textBoxDisplayURLs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDisplayURLs.Size = new System.Drawing.Size(1009, 500);
+            this.textBoxDisplayURLs.TabIndex = 3;
+            this.textBoxDisplayURLs.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1033, 656);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelStatus);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebScraper v0.9";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBoxDisplayURLs;
+    }
+}
+
---
+++ WebScraper/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraper
+{
+    public partial class Form1 : Form
+    {
+        readonly HttpClient client = new HttpClient();
+        Dictionary<Task<byte[]>, string> downloads = new Dictionary<Task<byte[]>, string>();
+        List<string> URLCollection = new List<string>();
+        private string WebsiteURL { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonExtract_Click_1(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+            await ExtractHTMLAsync(input);
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            await SavePicturesAsync(ReturnSavePath());
+        }
+
+        private string ImgExtension(string imgUrl)
+        {
+            Regex imgExtension = new Regex(@"\.jpg|\.jpeg|\.png|\.gif|\.bmp");
+            return imgExtension.Match(imgUrl).Value;
+        }
+
+        private string ReturnSavePath()
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.ShowDialog();
+
+            return folder.SelectedPath;
+        }
+
+        public string GetFullPath(string selectedSavePath, string filename)
+        {
+            return Path.Combine(selectedSavePath, filename);
+        }
+
+        private async Task<string> DownloadUrlAsync()
+        {
+            try
+            {
+                Task<string> download = client.GetStringAsync(WebsiteURL);
+                await download;
+
+                return download.Result;
+            }
+            catch
+            {
+                MessageBox.Show("The URL you requested could not be found.", "404 Error");
+                return null;
+            }
+        }
+
+        private async Task<List<string>> AddImgURLToListAsync(string downloadedUrl, CancellationToken cancellationToken)
+        {
+            Regex findImgTags = new Regex("(?<=<img[^h].+src=\")[h/].+?(.jpe?g|.png|.gif|.bmp)(?<=\"?)[^\"]*?(?=\".+>)");
+
+            Task addToList = Task.Run(() =>
+            {
+                var matches = findImgTags.Matches(downloadedUrl);
+                AddRegexMatchesToURLCollection(matches);
+            });
+            await addToList;
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new TaskCanceledException(addToList);
+            }
+
+            return URLCollection;
+        }
+
+        private async Task<bool> AddImgURLToListWithTimeoutAsync(TimeSpan timeSpan, string downloadedUrl)
+        {
+            using (var cancellationToken = new CancellationTokenSource(timeSpan))
+            {
+                try
+                {
+                    await AddImgURLToListAsync(downloadedUrl, cancellationToken.Token);
+                    return true;
+                }
+
+                catch (TaskCanceledException)
+                {
+                    MessageBox.Show("Timeout Exception", "Exception");
+                    return false;
+                }
+            }
+        }
+
+        private async Task SavePicturesAsync(string selectedSavePath)
+        {
+            if (selectedSavePath == "") { MessageBox.Show($"Save canceled.", "Canceled"); return; }
+
+            AddURLCollectionToDownloads();
+
+            int imgCounter = 0;
+
+            while (downloads.Count > 0)
+            {
+                Task<byte[]> completedTask = null;
+                try
+                {
+                    completedTask = await Task.WhenAny(downloads.Keys);
+                    string fullPath = GetFullPath(selectedSavePath, ImgName(imgCounter, ImgExtension(downloads[completedTask])));
+
+                    using (FileStream fs = File.Open($"{fullPath}", FileMode.Create))
+                    {
+                        await fs.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+                        labelStatus.Invoke(new Action(() => labelStatus.Text = $"Downloaded {imgCounter + 1} of {URLCollection.Count} images"));
+                        downloads.Remove(completedTask);
+                        imgCounter++;
+                    }
+                }
+                catch
+                {
+                    downloads.Remove(completedTask);
+                    continue;
+                }
+            }
+            MessageBox.Show($"Successfully saved {imgCounter} images.", "Success");
+        }
+
+        private async Task PrintImgURLAsync()
+        {
+            Task print = Task.Run(() =>
+            {
+                for (int i = 0; i < URLCollection.Count; i++)
+                {
+                    textBoxDisplayURLs.Invoke(new Action(() => textBoxDisplayURLs.Text += $"{URLCollection[i]}" + Environment.NewLine));
+                    labelStatus.Invoke(new Action(() => labelStatus.Text = $"Found {i + 1} images"));
+                }
+            });
+            await print;
+
+            if (print.Status == TaskStatus.RanToCompletion)
+            {
+                buttonSave.Enabled = true;
+            }
+        }
+
+        private async Task ExtractHTMLAsync(string input)
+        {
+            ResetApplication();
+            CheckIfInputContainsHttp(input);
+            var downloadedURL = await DownloadUrlAsync();
+
+            if (downloadedURL == null) return;
+
+            var result = await AddImgURLToListWithTimeoutAsync(TimeSpan.FromMilliseconds(10000), downloadedURL);
+
+            if (result == true)
+            {
+                await PrintImgURLAsync();
+            }
+        }
+
+        private void ResetApplication()
+        {
+            URLCollection.Clear();
+            textBoxDisplayURLs.Text = "";
+            buttonSave.Enabled = false;
+            labelStatus.Text = $"Found {0} images";
+        }
+
+        private void CheckIfInputContainsHttp(string input)
+        {
+            if (!input.Contains("http://"))
+                WebsiteURL = $"http://{input}";
+            else
+                WebsiteURL = input;
+        }
+
+        private string ImgName(int counter, string imgExtension)
+        {
+            string imageName = "Image" + (counter + 1) + imgExtension;
+            return imageName;
+        }
+
+        private void AddURLCollectionToDownloads()
+        {
+            for (int i = 0; i < URLCollection.Count; i++)
+            {
+                downloads.Add(client.GetByteArrayAsync($"{URLCollection[i]}"), URLCollection[i]);
+            }
+        }
+
+        private void AddRegexMatchesToURLCollection(MatchCollection matches)
+        {
+            for (int i = 0; i < matches.Count; i++)
+            {
+                if (!matches[i].ToString().Contains("http"))
+                {
+                    URLCollection.Add($"{WebsiteURL}{matches[i]}");
+                }
+                else
+                {
+                    URLCollection.Add($"{matches[i]}");
+                }
+            }
+        }
+
+    }
+}
